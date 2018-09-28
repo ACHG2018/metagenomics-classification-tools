41a41eb337a823bc0276e70845f07b0bf7bea342
Tomas Bruna <bruna.tomas@gmail.com>
2018-09-28 18:27:01 -0400

    Add option to create a CLARK database in which each db file is mapped to a unique, arbitrary taxonomic id

--- CLARK/db_setup.sh
+++ CLARK/db_setup.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 
 # Setup custom CLARK database
-# Usage: ./db_setup <reference-tax_id mapping> <db_folder>
+# Usage: ./db_setup <reference-tax_id mapping> <db_folder> [<unique>]
 #
 #        - <reference-tax_id mapping>   contig-tax_id mapping file
 #        - <db_folder>                  path to a folder with custom reference genomes
+#        - [<unique>]                   If this argument is specified, each database file is mapped to a unique taxID.
+#                                       Mapping taxonomic information is ignored.
 
 if [ $# -lt 2 ]; then
     >&2 echo "Invalid number of arguments"
-    >&2 echo "Usage: $0 <reference-tax_id mapping> <db_folder>"
+    >&2 echo "Usage: $0 <reference-tax_id mapping> <db_folder> [<unique>]"
     exit
 fi
 
@@ -30,9 +32,22 @@ cp $db/*.fasta DIR_DB/Custom
 # Reset any existing entries
 ./resetCustomDB.sh
 
-# Taxonomic ID mapping based on supplied mapping file
-paste <(ls DIR_DB/Custom/*.fasta) <(grep -P "CR_[0-9]+_Contig_0" \
-    $mapping | sort | cut -f1,2) > DIR_DB/.custom.fileToAccssnTaxID
+if [ $# -eq 3 ] && [ $3 == "unique" ]; then
+    >&2 echo "Building database with unique, arbitrary taxonomic IDs"
+    # Create file-accession mapping
+    paste <(ls DIR_DB/Custom/*.fasta | sort -V) <(grep -P "CR_[0-9]+_Contig_0" \
+        $mapping | sort -V | cut -f1) <(ls DIR_DB/Custom/*.fasta \
+        | sort -V | cut -f 3 -d_ | cut -f1 -d.) > DIR_DB/.custom.fileToAccssnTaxID
+    # Create custom empty hiearchy file
+    yes "UNKNOWN" | head -n $(wc -l DIR_DB/.custom.fileToAccssnTaxID | cut -f1 -d" ") > unknown
+    paste <(cut -f1,3 DIR_DB/.custom.fileToAccssnTaxID) <(cut -f3 DIR_DB/.custom.fileToAccssnTaxID) \
+        unknown unknown unknown unknown > DIR_DB/.custom.fileToTaxIDs
+    rm unknown
+else
+    >&2 echo "Building database with real taxonomic IDs"
+    paste <(ls DIR_DB/Custom/*.fasta) <(grep -P "CR_[0-9]+_Contig_0" \
+        $mapping | sort | cut -f1,2) > DIR_DB/.custom.fileToAccssnTaxID
+fi
 
 # Create the custom database
 ./set_targets.sh DIR_DB custom
