ef3a4238979807110e351ce24fcf1d7db6b1064c
dongjo <dongjoban17@gmail.com>
2018-10-16 02:18:35 -0400

    add Bracken installation

--- Kraken2/install.sh
+++ Kraken2/install.sh
@@ -5,10 +5,18 @@ cd $(dirname $0)
 
 # Download source code from github
 wget https://github.com/DerrickWood/kraken2/archive/master.zip
+wget https://github.com/jenniferlu717/Bracken.git
 
 unzip master.zip
+
 rm master.zip
 cd kraken2-master
 
 # Install Kraken2
 ./install_kraken2.sh $(dirname $0)
+
+cd $(dirname $0)
+cd Bracken
+./install_bracken.sh
+
+cd $(dirname $0)
